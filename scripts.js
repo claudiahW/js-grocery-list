a95fa2c85050664975989fa57b98c4b1295e8e24
claudiahW <murungiclaud@gmail.com>
2024-01-21 21:08:06 +0300

    select necessary items either through queryselectors or by id

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
 // ****** SELECT ITEMS **********
+// select all needed items this is always step one 
+
+const alert = document.querySelector(".alert");
+const form = document.querySelector(".grocery-form");
+const grocery = document.getElementById("grocery");
+const submitBtn = document.querySelector(".submit-btn");
+const container = document.querySelector(".grocery-container");
+const list = document.querySelector(".grocery-list");
+const clearBtn = document.querySelector(".clear-btn");
 
 // edit option
 
